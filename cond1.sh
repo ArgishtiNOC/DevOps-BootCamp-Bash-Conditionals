11164c411736af464874e64c30fb70eaecf505ea
ArgishtiNOC <80466645+ArgishtiNOC@users.noreply.github.com>
2022-12-28 23:36:11 +0400

    Update cond1.sh

--- cond1.sh
+++ cond1.sh
@@ -3,3 +3,11 @@
 IFS=',' read -r -a array <<< "$1"
 
 #Write your code here
+echo "Enter String"
+read Text
+symbols="${Text//[^*!@#$%^&()_+]/}"
+numbers="${Text//[^[:digit:]]/}"
+letters="${Text//[^[:alpha:]]/}"
+echo "Symbols= ${#symbols}"
+echo "Numbers= ${#numbers}"
+echo "Letters= ${#letters}"
