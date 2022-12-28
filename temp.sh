1ae6ebe416d0d17c5856f56b6260d147f7d2c605
ArgishtiNOC <80466645+ArgishtiNOC@users.noreply.github.com>
2022-12-28 23:35:41 +0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 # Place your code here
+c=$(echo $1 | grep -c "c")
+k=$(echo $1 | grep -c "k")
+
+
+#doing calculations
+if (( $c == "1" ));then
+   k=$(echo "scale=2;$c+273.15"|bc -l)
+   echo "$k k";
+elif (( $k == "1" ));then
+   c=$(echo "scale=1;$k-273.15" | bc -l)
+   echo "$c c";
+fi
