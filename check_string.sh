8f27edb0b955e9fba3fd8d496fb0dec152ec61f2
ArgishtiNOC <80466645+ArgishtiNOC@users.noreply.github.com>
2022-12-28 23:35:12 +0400

    Update check_string.sh

--- check_string.sh
+++ check_string.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 # Place your code here
+echo "Enter Size(N)"
+read N
+sum=0
+echo "Enter Numbers"
+for((i=1;i<=N;i++))
+do
+  read num           #get number
+  if [ `expr $num % 2` == 0 ]
+  then
+      sum=$((sum + num)) #sum+=num
+  fi
+done
+echo $sum
