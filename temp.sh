628c2d65455eddad1b1c80674123a1e209a193cc
ArgishtiNOC <80466645+ArgishtiNOC@users.noreply.github.com>
2022-12-29 20:08:34 +0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # Place your code here
-c=$(echo $1 | grep -c "c")
-k=$(echo $1 | grep -c "k")
+c=$(echo $1 | grep -c "C")
+k=$(echo $1 | grep -c "K")
 
 
 #doing calculations
 if (( $c == "1" ));then
-   k=$(echo "scale=2;$c+273.15"|bc -l)
-   echo "$k k";
+   c=$(echo $1 | sed 's/C//g');
+   k=$(echo "scale=2;$c+273" | bc -l)
+   echo "$k K";
 elif (( $k == "1" ));then
-   c=$(echo "scale=1;$k-273.15" | bc -l)
-   echo "$c c";
+   k=$(echo $1 | sed 's/K//g');
+   c=$(echo "scale=1;$k-273" | bc -l)
+   echo "$c C";
 fi
