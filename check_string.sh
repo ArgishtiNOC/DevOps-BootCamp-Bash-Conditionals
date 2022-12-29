4a2d79569f289f0041a1c3c79bfbff47bc0b4e3a
ArgishtiNOC <80466645+ArgishtiNOC@users.noreply.github.com>
2022-12-29 20:09:25 +0400

    Update check_string.sh

--- check_string.sh
+++ check_string.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 # Place your code here
-echo "Enter Size(N)"
-read N
-sum=0
-echo "Enter Numbers"
-for((i=1;i<=N;i++))
-do
-  read num           #get number
-  if [ `expr $num % 2` == 0 ]
-  then
-      sum=$((sum + num)) #sum+=num
-  fi
-done
-echo $sum
+x=${@}
+echo "$x"
+symbols="${x//[^*!@#$%^&()_+]/}"
+numbers="${x//[^[:digit:]]/}"
+letters="${x//[^[:alpha:]]/}"
+echo "Symbols= ${#symbols}"
+echo "Numbers= ${#numbers}"
+echo "Letters= ${#letters}"
