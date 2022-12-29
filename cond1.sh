52eff586e9802a5db28cc8514733e12716fd1b08
ArgishtiNOC <80466645+ArgishtiNOC@users.noreply.github.com>
2022-12-29 22:08:46 +0400

    Update cond1.sh

--- cond1.sh
+++ cond1.sh
@@ -3,11 +3,10 @@
 #IFS=',' read -r -a array <<< "$1"
 
 #Write your code here
+#!/bin/bash
 IFS=',' read -r -a nums <<< "$1"
-x=${#nums[@]}
-echo "$x"
 sum=0
-for arg in "${!nums[@]}"; do
+for arg in "${nums[@]}"; do
    if (( arg%2==0 )); then 
       sum=$((sum + arg))
     fi
