ff75ac187406d9d19e1628d023ba4802d1db3d8d
ArgishtiNOC <80466645+ArgishtiNOC@users.noreply.github.com>
2022-12-29 20:14:25 +0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-# Place your code here
 c=$(echo $1 | grep -c "C")
 k=$(echo $1 | grep -c "K")
 
@@ -9,9 +6,9 @@ k=$(echo $1 | grep -c "K")
 if (( $c == "1" ));then
    c=$(echo $1 | sed 's/C//g');
    k=$(echo "scale=2;$c+273" | bc -l)
-   echo "$k K";
+   echo "${k}K";
 elif (( $k == "1" ));then
    k=$(echo $1 | sed 's/K//g');
    c=$(echo "scale=1;$k-273" | bc -l)
-   echo "$c C";
+   echo "${c}C'";
 fi
