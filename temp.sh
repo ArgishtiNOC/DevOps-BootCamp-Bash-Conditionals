2921e8b12554868757af903bdcbdca629081beb0
ArgishtiNOC <80466645+ArgishtiNOC@users.noreply.github.com>
2022-12-30 20:34:56 +0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,14 +1,12 @@
-c=$(echo $1 | grep -c "C")
-k=$(echo $1 | grep -c "K")
-
-
-#doing calculations
-if (( $c == "1" ));then
-   c=$(echo $1 | sed 's/C//g');
+#!/bin/bash
+c=$(echo "$1" | grep -c "C")
+k=$(echo "$1" | grep -c "K")
+if (( "$c" == "1" ));then
+   c=$( echo "$1" | sed 's/C//g' );
    k=$(echo "scale=2;$c+273" | bc -l)
    echo "${k}K";
-elif (( $k == "1" ));then
-   k=$(echo $1 | sed 's/K//g');
+elif (( "$k" == "1" ));then
+   k=$(echo "$1" | sed 's/K//g');
    c=$(echo "scale=1;$k-273" | bc -l)
    echo "${c}C'";
 fi
